5edf2774a0a0febb92181d798e735e6e4429bb56
Makis Tracend <makis.tracend@gmail.com>
2013-01-30 04:56:30 -0800

    better calculation of rotation properties

--- examples/assets/js/libs/FlyControls.js
+++ examples/assets/js/libs/FlyControls.js
@@ -194,12 +194,14 @@ THREE.FlyControls = function ( object, domElement ) {
 		this.tmpQuaternion.set( this.rotationVector.x * rotMult, this.rotationVector.y * rotMult, this.rotationVector.z * rotMult, 1 ).normalize();
 		this.object.quaternion.multiply( this.tmpQuaternion );
 
-		this.object.matrix.setPosition( this.object.position );
-		this.object.matrix.setRotationFromQuaternion( this.object.quaternion );
-		this.object.matrixWorldNeedsUpdate = true;
+		//this.object.matrix.setPosition( this.object.position );
+		//this.object.matrix.setRotationFromQuaternion( this.object.quaternion );
+		//this.object.matrixWorldNeedsUpdate = true;
 		
+		// expose the rotation vector for convenience
+		this.object.rotation.setEulerFromQuaternion( this.object.quaternion, this.object.eulerOrder );
 		// reflect rotation to the objects properties
-		this.object.rotation.setEulerFromRotationMatrix( this.object.matrix );
+		//this.object.rotation.setEulerFromRotationMatrix( this.object.matrix );
 		
 	};
 
