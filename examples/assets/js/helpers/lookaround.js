81ba32d5eeb18ac0789f84b495aa3b511bec8c39
Makis Tracend <makis.tracend@gmail.com>
2013-09-30 20:29:33 -0700

    precautionary condition

--- examples/assets/js/helpers/lookaround.js
+++ examples/assets/js/helpers/lookaround.js
@@ -3,17 +3,19 @@
 // variables
 var mouseX = 0;
 var mouseY = 0;
-	
+
 LookArround = function(e){
-					
-		// the 3d environment is passed as target 
+
+		// the 3d environment is passed as target
 		var $3d = e.target;
-		
+
+		// start when the active camera is set
+		if( !$3d.active.camera ) return;
 		$3d.active.camera.lookAt( $3d.active.scene.position );
-		
+
 		$3d.active.camera.position.x += ( mouseX - $3d.active.camera.position.x ) * 0.05;
 		$3d.active.camera.position.y += ( - mouseY - $3d.active.camera.position.y ) * 0.05;
-		
+
 }
 
 // add mouse tracking
@@ -23,7 +25,7 @@ function onDocumentMouseMove(event) {
 
 	mouseX = ( event.clientX - (window.innerWidth / 2) ) * 4;
 	mouseY = ( event.clientY - (window.innerHeight / 2) ) * 4;
-	
+
 }
-	
+
 })();
