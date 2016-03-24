5c22a36849ee5f94614c2353a12a19aea5b06e0a
Makis Tracend <makis.tracend@gmail.com>
2016-03-24 03:23:21 -0700

    :wrench: palm mesh

--- examples/assets/3d/palm/mesh.js
+++ examples/assets/3d/palm/mesh.js
@@ -1,45 +1,54 @@
 {
-
-	"metadata" :
-	{
-		"formatVersion" : 3.1,
-		"sourceFile"    : "palm.obj",
-		"generatedBy"   : "OBJConverter",
-		"vertices"      : 534,
-		"faces"         : 398,
-		"normals"       : 490,
-		"uvs"           : 147,
-		"materials"     : 2
+	"metadata": {
+		"formatVersion": 0.6,
+		"sourceFile": "palm.obj",
+		"generatedBy": "three-obj",
+		"vertices": 537,
+		"faces": 399,
+		"normals": 490,
+		"colors": 0,
+		"uvs": 150,
+		"materials": 3
 	},
 
 	"materials": [{
-	"DbgColor" : 15658734,
-	"DbgIndex" : 0,
-	"DbgName" : "trunk",
-	"colorAmbient" : [0.8, 0.8, 0.8],
-	"colorDiffuse" : [1.0, 1.0, 1.0],
-	"colorSpecular" : [1.0, 1.0, 1.0],
-	"illumination" : 2,
-	"mapBump" : "maps/palm_bark_norm.jpg",
-	"mapDiffuse" : "maps/palm_bark.jpg",
-	"specularCoef" : 8.0,
-	"transparent" : false
-	},
-
-	{
-	"DbgColor" : 15597568,
-	"DbgIndex" : 1,
-	"DbgName" : "branches",
-	"colorAmbient" : [0.8, 0.8, 0.8],
-	"colorDiffuse" : [1.0, 1.0, 1.0],
-	"colorSpecular" : [1.0, 1.0, 1.0],
-	"illumination" : 2,
-	"mapDiffuse" : "maps/palm_leafs.png",
-	"specularCoef" : 8.0,
-	"transparency" : 1.0,
-	"transparent" : true
+		"DbgName": "coconuts",
+		"DbgIndex": 0,
+		"DbgColor": 15597568,
+		"colorAmbient" : [0.8, 0.8, 0.8],
+		"colorDiffuse": [1, 1, 1],
+		"colorSpecular": [1, 1, 1],
+		"illumination": 2,
+		"specularCoef": 8,
+		"mapDiffuse": "maps/palm_leafs.png",
+		"mapSpecular": "maps/palm_leafs_spec.jpg",
+		"transparent" : false
+	}, {
+		"DbgName": "trunk",
+		"DbgIndex": 1,
+		"DbgColor": 15658734,
+		"colorAmbient" : [0.8, 0.8, 0.8],
+		"colorDiffuse": [1, 1, 1],
+		"colorSpecular": [1, 1, 1],
+		"illumination": 2,
+		"specularCoef": 8,
+		"mapDiffuse": "maps/palm_bark.jpg",
+		"mapBump": "maps/palm_bark_norm.jpg",
+		"mapSpecular": "maps/palm_bark_spec.jpg",
+		"transparent" : false
+	}, {
+		"DbgName": "branches",
+		"DbgIndex": 2,
+		"DbgColor": 60928,
+		"colorAmbient" : [0.8, 0.8, 0.8],
+		"colorDiffuse": [1, 1, 1],
+		"colorSpecular": [1, 1, 1],
+		"illumination": 2,
+		"specularCoef": 8,
+		"mapDiffuse": "maps/palm_leafs.png",
+		"mapSpecular": "maps/palm_leafs_spec.jpg",
+		"transparency" : 1.0,
+		"transparent" : true
 	}],
-
 	"buffers": "mesh.bin"
-
 }
