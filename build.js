bfe6ebdd377306df6480c3fe780de388c14ea98c
Makis Tracend <makis.tracend@gmail.com>
2016-04-15 21:00:31 -0700

    preserving comments in minified build

--- build.js
+++ build.js
@@ -84,9 +84,12 @@ function uglify(srcPath, distPath) {
 	ast = pro.ast_squeeze(ast);
 	*/
 
-	var result = _uglify.minify(srcPath, { compressor: {
-		comments : true
-	} });
+	var result = _uglify.minify(srcPath, {
+		mangle: true,
+		output: {
+			comments : /@name|@author|@cc_on|@url|@license/
+		}
+	});
 
 	_fs.writeFileSync(distPath, result.code, FILE_ENCODING);
 	console.log(' '+ distPath +' built.');
